8fe054fd2d2f8c97d65d7f528e7e80598b41e819
matsu-chara <yu_matsubayashi@dwango.co.jp>
2015-06-20 21:53:34 +0900

    init

---
+++ README.md
@@ -0,0 +1,124 @@
+# enjo.js
+
+> jQueryしか使えない！そんなときでも大丈夫。
+
+enjo.jsはjQueryに依存した双方向データバインディングができるMVVMフレームワークです。
+
+たぶん・・・( ⁰⊖⁰)
+
+勉強用なので実用性はありません。プロダクトで使うと炎上します。
+
+
+## 使い方
+
+### 初期化
+
+enjoはjQueryに依存しているので
+
+`var e = enjo($);`としてjQueryを渡しましょう。
+あとは`e.Repository`や`e.Model()`, `e.ViewModel`をそれぞれのオブジェクトに
+渡してあげればOkです。
+
+```javascript
+$(document).ready(function(){
+  // dependent views
+  var $containerView = $("#todo-container");
+  var $listView = $containerView.children("#todo-list");
+  var $formView = $containerView.children("#todo-form");
+
+  var e = enjo($);
+  var todoRepository = todo.Repository(e.Repository());
+  var todoModel = todo.Model(todoRepository, e.Model());
+  todo.ListViewModel($listView, todoModel, e.ViewModel());
+  todo.FormViewModel($formView, todoModel, e.ViewModel());
+
+  todoModel.init();
+});
+```
+
+### Repository
+
+MVVMなのに何故かRepositoryがありますが、これは`$.ajax`の薄い（そして不完全な）ラッパーですので、使わなくても大丈夫です。
+
+下記のようにコールバックを指定して簡単にリクエストを飛ばすことが出来ます。
+
+```javascript
+enjoR.get("/todo", done);
+```
+
+ｲﾁｵｳﾍﾞﾝﾘ( ⁰⊖⁰)
+
+### Model
+
+モデルの値は専用の`getter/setter`で管理します。
+setterで値を変更すると`enjo-update`というイベントが飛ぶので、
+それをViewModelで取得してViewを書き換えることが出来ます。
+
+```
+var that = {};
+var enjoM = enjo($).Model();
+
+/*
+ * 値の更新時に$(bindParamsの第一引数).trigger("enjo-update", bindParamsの第二引数)という
+ * イベントが飛ぶ。
+ */
+var fire = enjoM.bindParams(that, {
+  todos: []
+});
+
+// getの例
+var t = fire.get("todos");
+
+// setの例
+fire.set("todos", t);
+};
+```
+
+### ViewModel
+
+ViewとBindしたい値をセットにして、bindParamsメソッドに渡せば、
+Viewのchangedイベントを取得して、値を自動的に反映してくれます。
+また、`fire.set`メソッドで値を更新すれば、viewにも自動的に反映してくれます。
+
+好きなViewに対してカスタムeventとコールバックをbindすることも可能です。
+
+
+```javascript
+var enjoVm = enjo($).ViewModel();
+
+var fire = enjoVm.bindParams({
+    url: {
+    $view: $url,
+    value: "http://google.com"
+    },
+    content: {
+    $view: $content,
+    value: "テストマーキー"
+    },
+    submit: {
+    $view: $submit,
+    event: "click",
+    callback: function(e, fire) {
+        e.preventDefault();
+        var m = model.newtodo(
+        fire.get("url"),
+        fire.get("content")
+        );
+        if(model.validate(m)) {
+        model.add(m);
+        }
+    }
+    }
+});
+```
+
+Viewからの変更ではなくてモデルの変更を受け取りたい場合は、
+constructorで`enjoVm.init`よんであげましょう。
+変更を受け取りたいモデルと、コールバックを渡すと、
+モデルの変更のたびにコールバックが呼ばれます。
+
+```javascript
+function construct() {
+    enjoVm.init(model, onUpdate);
+}
+```
---
+++ demo.js
@@ -0,0 +1,210 @@
+(function() {
+  "use strict";
+
+  // namespace todo
+  var todo = todo || {};
+
+  todo.Repository = function(enjoR) {
+    var that = {};
+
+    that.gettodos = function(done) {
+      // enjoR.get("/todo", done);
+    };
+
+    that.posttodo = function(todo, done) {
+      setTimeout(function(){
+        // enjoR.post("/todo", todo, done);
+      }, 1000);
+    };
+
+    that.deletetodo = function(todo, done) {
+      // enjoR.delete("/todo/" + todo.id, done);
+    };
+    return that;
+  };
+
+  todo.Model = function(repository, enjoM) {
+    var that = {};
+
+    var fire = enjoM.bindParams(that, {
+      todos: []
+    });
+
+    that.init = function() {
+      // 初回更新
+      update();
+
+      // 定期的に更新
+      setInterval(update, 3 * 1000);
+    };
+
+    that.newtodo = function(url, content) {
+      return {id: null, url: url, content: content};
+    };
+
+    that.isRegistering = function(todo) {
+      return todo.id === null;
+    };
+
+    that.remove = function(todo) {
+      if (that.isRegistering(todo)) {
+        // return;
+      }
+
+      // 対象要素以外を抽出する形で更新
+      var notDeletingtodos = fire.get("todos").filter(function(m) {
+        return m !== todo;
+      });
+      fire.set("todos", notDeletingtodos);
+
+      repository.deletetodo(todo);
+    };
+
+    that.add = function(todo) {
+      fire.set("todos", fire.get("todos").concat(todo));
+
+      repository.posttodo(todo, function(registeredtodo){
+        todo.id = registeredtodo.id;
+        update();
+      });
+    };
+
+    that.validate = function (todo) {
+      var isValid = (todo.content !== "" && todo.url.match("^http"));
+      return isValid;
+    };
+
+    function update() {
+      repository.gettodos(function(_todos){
+        var todos = fire.get("todos");
+
+        // 登録中のものを上書きしないように最新のリストを取得
+        var registeringtodos = todos.filter(that.isRegistering);
+        fire.set("todos", _todos.concat(registeringtodos));
+      });
+    }
+
+    return that;
+  };
+
+  todo.FormViewModel = function($view, model, enjoVm) {
+    // view dependencies
+    var $url     = $view.find("#todo-url");
+    var $content = $view.find("#todo-content");
+    var $submit  = $view.find("#todo-submit");
+
+    // viewとbindされた値(setで値を更新すればViewにも反映される)
+    // eventとコールバックをbindすることも可能
+    var fire = enjoVm.bindParams({
+      url: {
+        $view: $url,
+        value: "http://google.com"
+      },
+      content: {
+        $view: $content,
+        value: "テストマーキー"
+      },
+      submit: {
+        $view: $submit,
+        event: "click",
+        callback: function(e, fire) {
+          e.preventDefault();
+          var m = model.newtodo(
+            fire.get("url"),
+            fire.get("content")
+          );
+          if(model.validate(m)) {
+            model.add(m);
+          }
+        }
+      }
+    });
+
+    function construct() {
+      render();
+      enjoVm.init(model, onUpdate);
+    }
+
+    function render() {
+    }
+
+    function onUpdate(event, data) {
+    }
+
+    construct();
+  };
+
+  todo.ListViewModel = function($view, model, enjoVm) {
+    function construct() {
+      render([]);
+      enjoVm.init(model, onUpdate);
+    }
+
+    function render(todos) {
+      var ul = createtodoList(todos);
+      $view.html(ul);
+    }
+
+    function onUpdate(event, data) {
+      render(data.todos);
+    }
+
+    // マーキー配列をulに変換
+    function createtodoList(todos) {
+      var ul = $("<ul>").attr("id", "todo-ul");
+
+      todos.forEach(function(todo) {
+        var li = createtodoItem(todo);
+        ul.append(li);
+      });
+      return ul;
+    }
+
+    // マーキーをliに変換
+    function createtodoItem(todo) {
+      var li = $("<li>").addClass("todo-item");
+
+      var deleteButton = $("<button>")
+                          .addClass("todo-delete")
+                          .text("削除")
+                          .click(function() {
+                            model.remove(todo);
+                          });
+
+      var span = $("<span>")
+                  .addClass("todo-content")
+                  .text(todo.content + ": ");
+
+      var a = $("<a>")
+                .addClass("todo-anchor")
+                .attr("href", todo.url)
+                .attr("target", "_blank")
+                .text(todo.url);
+
+      /*
+        <li>
+          <button>
+          <span><a></span>
+        </li>
+      */
+      return li.append(deleteButton).append(span.append(a));
+    }
+
+    construct();
+  };
+
+  $(document).ready(function(){
+    // dependent views
+    var $containerView = $("#todo-container");
+    var $listView = $containerView.children("#todo-list");
+    var $formView = $containerView.children("#todo-form");
+
+    var e = enjo($);
+    var todoRepository = todo.Repository(e.Repository());
+    var todoModel = todo.Model(todoRepository, e.Model());
+    todo.ListViewModel($listView, todoModel, e.ViewModel());
+    todo.FormViewModel($formView, todoModel, e.ViewModel());
+
+    todoModel.init();
+  });
+}());
